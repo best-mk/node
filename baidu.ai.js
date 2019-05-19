c22016f8cf8e6b69308c44125f13cd8cc6fec2e7
mike <376130322@163.com>
2019-05-19 15:45:24 +0800

    查天气智能问答

--- baidu.ai.js
+++ baidu.ai.js
@@ -9,11 +9,28 @@ const ai=express();
 ai.use('/web', express.static('web'));
 ai.use(cookieParser());
  //apppid 16070855
+var token = ''
 const param = qs.stringify({
     'grant_type': 'client_credentials',
-    'client_id': 'l08MwyMLLeI5P4HvZj5SapkY',
-    'client_secret': 'Rt3GmAHzrGOwU9bovblTjHubUCG9gW7C'
+    'client_id': 'QC8rHYEA2EvZEtP5BxaffKQA',
+    'client_secret': '7hWggxufIODfPgqZX63rrXO18OpS3nug'
 });
+https.get(
+    {
+        hostname: 'aip.baidubce.com',
+        path: '/oauth/2.0/token?' + param,
+        agent: false
+    },
+    function (res) {
+        res.on('data', (d) => {
+			console.log("认证成功")
+			let b = JSON.parse('' + d); //将buffer转成JSON
+			token = b["access_token"]
+			console.log(token)
+		})
+    }
+);
+
 ai.use(bodyParser.urlencoded({
     extended: false
 }));
@@ -31,58 +48,55 @@ ai.use("*", function(request, response, next) {
 // ai.get("/acctoken",function(req,res){
 
 // });
+var logId = 1000
 ai.post("/sendMsg",function(req,res){
 	var text1 = req.body.text
-	https.get({
-		hostname: 'aip.baidubce.com',
-		path: '/oauth/2.0/token?' + param,
-		agent: false
-	},function (res2) {
-		// 在标准输出中查看运行结果
-		// res2.pipe(process.stdout);
-		// console.log('statusCode:', res2.statusCode);
-		// console.log('headers:', res2.headers);
-		res2.on('data', (d) => {
-			console.log("认证成功")
-			console.log('' + d); //将buffer转为字符串或者使用d.toString()
-			let b = JSON.parse('' + d); //将buffer转成JSON
-			console.log(b)
-			var access_token = b["access_token"]
-			console.log(access_token)
-			// res.end(JSON.stringify(b));
-			request_url = "https://aip.baidubce.com/rpc/2.0/nlp/v2/simnet" + "?access_token=" + access_token
-			// var list = ['出险了去哪理赔？','旅行买什么保险？','出国买什么保险？','哪些保险好？']
-			var list = '出险了去哪理赔？'
-			var textObj = []
-			// list.forEach(function(item,index){
-			// 	let text2 = item
-				request({
-					url: request_url,
-					method: "POST",
-					json: true,
-					headers: {
-							"content-type": "application/json",
-					},
-					body: {text_1:text1,text_2:'吃饭了吗?'}
-				}, function (error, response, body) {
-						console.log(error)
-						// console.log(response)
-						// console.log(body)
-						if (!error && response.statusCode == 200) {
-								console.log(body) // 请求成功的处理逻辑
-								// if(textObj.length < list.length){
-								// 	textObj.push({'score':body.score,'text':body.texts.text_2})
-								// }else{
-									// res.end(JSON.stringify(textObj[0].text));
-								// }
-								res.end(JSON.stringify(body));
-						}
-					});
-				});
-			// })
-		});
-		// res.cookie('token',token)
-    // res.send({"code": "0", "text": req.body.text})
+	logId + 1
+	console.log(text1)
+	var setData = {
+		'log_id': logId,
+		'version': '2.0',
+		'request': {
+			'user_id': '88888',
+			'query_info': {
+				'asr_candidates': [],
+				'type': 'TEXT',
+				'source': 'KEYBOARD'
+			 },
+			 'bernard_level': 1, 
+			 'updates': '',
+			 'query': text1,
+			 'client_session': '{"client_results":"", "candidate_options":[]}'
+		},
+		'bot_session': '',
+		'bot_id': '49631'
+	}
+	if(req.body.session_id){
+		setData.bot_session = JSON.stringify({'session_id':req.body.session_id})
+	}
+	request(
+		{
+			url: 'https://aip.baidubce.com/rpc/2.0/unit/bot/chat?access_token=' + token,
+			method: 'POST',
+			json: true,
+			headers: {
+				'Content-Type': 'application/json; charset=UTF-8'
+			},
+			body:setData
+		},
+		function (error, response, body) {
+			// 在标准输出中查看运行结果
+			if (!error && response.statusCode == 200) {
+				console.log(body) // 请求成功的处理逻辑
+				// if(textObj.length < list.length){
+				// 	textObj.push({'score':body.score,'text':body.texts.text_2})
+				// }else{
+					// res.end(JSON.stringify(textObj[0].text));
+				// }
+				res.end(JSON.stringify(body));
+			}
+		}
+	)
 });
 
  
